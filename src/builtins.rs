269cf198337fc2c753391b0ebca7f5b976139ed6
joshmeranda <joshmeranda@gmail.com>
2021-11-02 00:03:21 -0400

    add argument handling macro

--- src/builtins.rs
+++ src/builtins.rs
@@ -2,6 +2,26 @@ use std::path::PathBuf;
 
 use clap::{Arg, ErrorKind};
 
+/// handle_matches is designed to allow for clean and uniform argument handling.
+macro_rules! handle_matches {
+    ($app:ident, $argv:ident) => {
+        match $app.get_matches_from_safe($argv) {
+            Err(err) => match err.kind {
+                ErrorKind::HelpDisplayed => {
+                    println!("{}", err);
+                    return;
+                }
+                _ => {
+                    eprintln!("Error: '{}", err);
+                    return;
+                }
+            },
+            Ok(m) => m,
+        }
+    }
+}
+
+/// Exit is a builtin for exiting out of the current shell session.
 pub fn exit(argv: &[String]) {
     let app = app_from_crate!().name("exit").arg(
         Arg::with_name("code")
@@ -16,25 +36,14 @@ pub fn exit(argv: &[String]) {
             }),
     );
 
-    let matches = match app.get_matches_from_safe(argv) {
-        Err(err) => match err.kind {
-            ErrorKind::HelpDisplayed => {
-                println!("{}", err);
-                return;
-            }
-            _ => {
-                eprintln!("Error: '{}", err);
-                return;
-            }
-        },
-        Ok(m) => m,
-    };
+    let matches = handle_matches!(app, argv);
 
     let code: i32 = matches.value_of("code").unwrap().parse().unwrap();
 
     std::process::exit(code);
 }
 
+/// CD is builtin for changing the current working directory in the shell.
 pub fn cd(argv: &[String]) {
     let app = app_from_crate!().name("cd").arg(
         Arg::with_name("directory")
@@ -51,19 +60,7 @@ pub fn cd(argv: &[String]) {
             }),
     );
 
-    let matches = match app.get_matches_from_safe(argv) {
-        Err(err) => match err.kind {
-            ErrorKind::HelpDisplayed => {
-                println!("{}", err);
-                return;
-            }
-            _ => {
-                eprintln!("Error: '{}", err);
-                return;
-            }
-        },
-        Ok(m) => m,
-    };
+    let matches = handle_matches!(app, argv);
 
     let target = matches.value_of("directory").unwrap();
 
